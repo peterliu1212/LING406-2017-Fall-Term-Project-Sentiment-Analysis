375bd5c39fbd289da82cc034ca6a110b5de698f7
LiuChengYang <peterliu1212@gmail.com>
2017-05-05 01:02:53 -0500

    add function
    add read_acy, plot_acy functions

--- plot_bow_feature_number.py
+++ plot_bow_feature_number.py
@@ -1,41 +1,51 @@
 import re
 import matplotlib.pyplot as plt
 
-acy_nb = []
-acy_lg = []
-acy_dt = []
-acy_svm = []
-parameter = []
+def read_acy(file_name):
+	acy_nb = []
+	acy_lg = []
+	acy_dt = []
+	acy_svm = []
+	parameter = []
 
-dataset = 0 # movie review
-# dataset = 1 # yelp review
+	with open(file_name) as f:
+		for line in f:
+			data = line.split()
+			para = re.findall(r'^[0-9][0-9][0-9][0-9]?[0-9]?', data[0])
+			nb = re.findall(r'^[0-9][0-9][.][0-9][0-9]?', data[1])
+			lg = re.findall(r'^[0-9][0-9][.][0-9][0-9]?', data[2])
+			dt = re.findall(r'^[0-9][0-9][.][0-9][0-9]?', data[3])
+			svm = re.findall(r'^[0-9][0-9][.][0-9][0-9]?', data[4])
 
-with open('movie_bow_100_30000.txt') as f:
-	for line in f:
-		data = line.split()
-		para = re.findall(r'^[0-9][0-9][0-9][0-9]?[0-9]?', data[0])
-		nb = re.findall(r'^[0-9][0-9][.][0-9][0-9]?', data[1])
-		lg = re.findall(r'^[0-9][0-9][.][0-9][0-9]?', data[2])
-		dt = re.findall(r'^[0-9][0-9][.][0-9][0-9]?', data[3])
-		svm = re.findall(r'^[0-9][0-9][.][0-9][0-9]?', data[4])
+			acy_nb.append(float(''.join(nb)))
+			acy_lg.append(float(''.join(lg)))
+			acy_dt.append(float(''.join(dt)))
+			acy_svm.append(float(''.join(svm)))
+			parameter.append(int(''.join(para)))
 
-		acy_nb.append(float(''.join(nb)))
-		acy_lg.append(float(''.join(lg)))
-		acy_dt.append(float(''.join(dt)))
-		acy_svm.append(float(''.join(svm)))
-		parameter.append(int(''.join(para)))
+	return [parameter, acy_nb, acy_lg, acy_dt, acy_svm]
 
-plt.plot(parameter, acy_nb, 'bo')
-p1, = plt.plot(parameter, acy_nb, 'b')
-plt.plot(parameter, acy_lg, 'ko')
-p2, = plt.plot(parameter, acy_lg, 'k')
-plt.plot(parameter, acy_dt, 'ro')
-p3, = plt.plot(parameter, acy_dt, 'r')
-plt.plot(parameter, acy_svm, 'go')
-p4, = plt.plot(parameter, acy_svm, 'g')
-plt.legend([p1, p2, p3, p4], ['Naive Bayes', 'Logistic Regression', 'Decision Tree', 'Support Vector Machine'])
-plt.axis([0, 30000, 0, 100])
-plt.ylabel('Accuracy')
-plt.xlabel('Feature Size')
-plt.title('Baseline: Bag of Word feature')
-plt.show()
+def plot_acy(acy, title):
+	plt.plot(acy[0], acy[1], 'bo')
+	p1, = plt.plot(acy[0], acy[1], 'b')
+	plt.plot(acy[0], acy[2], 'ko')
+	p2, = plt.plot(acy[0], acy[2], 'k')
+	plt.plot(acy[0], acy[3], 'ro')
+	p3, = plt.plot(acy[0], acy[3], 'r')
+	plt.plot(acy[0], acy[4], 'go')
+	p4, = plt.plot(acy[0], acy[4], 'g')
+	plt.legend([p1, p2, p3, p4], ['Naive Bayes', 'Logistic Regression', 'Decision Tree', 'Support Vector Machine'])
+	plt.axis([0, 30000, 0, 100])
+	plt.ylabel('Accuracy')
+	plt.xlabel('Feature Size')
+	plt.title(title)
+	plt.show()
+
+file_movie_bow = 'movie_bow_100_30000.txt'
+acy_movie_bow = read_acy(file_movie_bow)
+title_movie_bow = 'Movie Baseline: Bag of Word feature'
+plot_acy(acy_movie_bow, title_movie_bow)
+# file_yelp_bow = 'yelp_bow_100_30000.txt'
+# yelp_bow = read_acy(file_yelp_bow)
+
+
